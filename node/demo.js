705d7d171bf8eeebe50e7b0b2256531f4b5e7501
badManPro <2529670555@qq.com>
2017-10-17 10:47:32 +0800

    add new homework

--- node/demo.js
+++ node/demo.js
@@ -1,14 +1,14 @@
  var fs = require('fs')
 
  var dirName = process.argv[2] 
-
+ var path = "./" + dirName
  process.chdir("/Users/Lucifer/Documents/Git/Script/node") // cd 目录（路径自己改）
-//  if ("./") {
-//     console.log("error");
-//     process.exit(0)
-//  } else {
-    fs.mkdirSync("./" + dirName) // mkdir $1
-    process.chdir("./" + dirName) // cd $1
+  if (fs.existsSync(path)) {
+     console.log("existence address");
+     process.exit(1)
+  } else {
+    fs.mkdirSync(path) // mkdir $1
+    process.chdir(path) // cd $1
     fs.mkdirSync('css') // mkdir css
     fs.mkdirSync('js') // mkdir js
    
@@ -16,5 +16,5 @@
     fs.writeFileSync("css/style.css", "h1{color: red;}")
     fs.writeFileSync("./js/main.js", 'var string = "Hello World"\nalert(string)')
     process.exit(0)   
-//  }
+}
  
