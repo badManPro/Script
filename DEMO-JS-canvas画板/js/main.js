95ffc8654ed8c9746f9c90ac17d4c939fee2f49d
badManPro <2529670555@qq.com>
2017-11-20 23:10:08 +0800

    fix bug demo-canvas

--- DEMO-JS-canvas画板/js/main.js
+++ DEMO-JS-canvas画板/js/main.js
@@ -11,7 +11,7 @@ listenToMouse(yyy);
 var eraserEnabled = false;
 // 橡皮擦启动函数
 eraser.onclick = function() {
-    eraserEnabled = ture;
+    eraserEnabled = true;
     actions.className = 'actions x'
 }
 //  画笔启动状态
@@ -38,11 +38,64 @@ function autoSetCanvasSize(canvas) {
         canvas.height = pageHeight;
     }
 }
-function drawCircle(x, y, radius) {
+
+// 画点
+function drawPoint(x, y, radius) {
     context.beginPath();
     context.fillStyle = 'black';
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill();
 }
 
+//  点与点之间连线（使得划线连贯）
+function drawLine(x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.moveTo(x1, y1); //起点
+    context.lineWidth = 5;
+    context.lineTo(x2, y2); //终点
+    context.stroke();
+    context.closePath();
+}
 
+//  
+function listenToMouse(canvas) {
+    var using = false;
+    var lastPoint = {
+        x: undefined,
+        y: undefined
+    }
+    canvas.onmousedown = function(aaa){
+        var x = aaa.clientX;
+        var y = aaa.clientY;
+        using = true;
+        if (eraserEnabled) {
+            context.clearRect(x - 5, y-5, 10, 10);
+        }else {
+            lastPoint = {
+                "x": x,
+                "y": y
+            }
+        }
+    }
+    canvas.onmousemove = function(aaa) {
+        var x = aaa.clientX;
+        var y = aaa.clientY;
+        if (!using) {
+            return;
+        }
+        if (eraserEnabled) {
+            context.clearRect(x - 5, y-5, 10, 10);
+        }else {
+            var newPoint = {
+                "x": x,
+                "y": y
+            }
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+            lastPoint = newPoint;
+        }
+    }
+    canvas.onmouseup = function(aaa){
+        using = false;
+    }
+}
